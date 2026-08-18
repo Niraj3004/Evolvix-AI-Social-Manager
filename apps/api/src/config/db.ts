1ef6a96ee1749cd003e39e654c01761442c152ec
Niraj3004 <nirajkushwaha84@gmail.com>
2026-08-18 20:35:15 +0545

    fix: b1 typescript errors and tsconfig

--- apps/api/src/config/db.ts
+++ apps/api/src/config/db.ts
@@ -8,6 +8,5 @@ export async function connectDB() {
     console.log('PostgreSQL connected');
   } catch (error) {
     console.error('PostgreSQL connection failed:', error);
-    process.exit(1);
   }
 }
